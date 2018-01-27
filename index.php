2492981f7faba4d60275ec7e878e3963c83d9b9c
ZueScious <ashishklingwal@gmail.com>
2018-01-27 16:16:28 +0530

    index page changes

--- index.php
+++ index.php
@@ -2,18 +2,20 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title> </title>
+<title></title>
 <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
 </head>
-<img src="HTB1jXHuGVXXXXb9apXXq6xXFXXXM.jpg" id="bg" alt="">
+
 <body>
- <div class="icon" align="center"></div>
+    <div class="header">< Choice List ></div>
+    <p class="disc">Paste the link here and save it on your playlist. Watch videos, images, listen to songs. No need to download when you can just save them. EZ :D</p>
   <div class="search">
         <form action="mng.php" method="post">
            <input type="text" class="ui_field" placeholder="Find a list, or make a new one" name="name">
            <button class="ui_button">Find/Make</button>
         </form>
    </div>
-
+    
 </body>
 </html>
